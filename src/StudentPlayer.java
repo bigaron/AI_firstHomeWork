b2da0918575b971bf434f4790bf79438aa34d258
bigaron <bigaron2@gmail.com>
2023-12-15 12:36:38 +0100

    finish in a few

--- src/StudentPlayer.java
+++ src/StudentPlayer.java
@@ -1,6 +1,7 @@
 public class StudentPlayer extends Player{
-    private int MAX_DEPTH = 2;
+    private int MAX_DEPTH = 3;
     private int enemyIndex;
+
 
     public StudentPlayer(int playerIndex, int[] boardSize, int nToConnect) {
         super(playerIndex, boardSize, nToConnect);
@@ -12,42 +13,65 @@ public class StudentPlayer extends Player{
         return maxScore(board, -Integer.MAX_VALUE, Integer.MAX_VALUE, 0)[0];
     }
 
-    private int longestCon(Board board, int index){
-        int usedTiles = 0;
-        int longestCon = 0;
-        int[][] state = board.getState();
-        for(int row = 0; row < boardSize[0]; ++row){
-            for(int col = 0; col < boardSize[1]; ++col){
-                for(int con = 1; con < 5; con++){
-                    if(isNDiagonally(row, col, index, con, state) || isNSkewDiagonally(row, col, index, con, state) 
-                        || isNInACol(row, col, index, con, state) || isNInARow(row, col, index, con, state)) if(con > longestCon) longestCon = con;
-                    if(state[row][col] != 0) usedTiles++;    
-                }
-            }
-        }
-
-        if(usedTiles == boardSize[0] * boardSize[1]) return -1;
-
-        return longestCon;
+    private int getRockCount(int[] slots, int indx){
+        int cnt = 0;
+        for(int i = 0; i < slots.length; ++i) if(slots[i] == indx) cnt++;
+        return cnt;
     }
 
-    private int utility(Board board){
+    private int score(int countP, int countE){
         int value = 0;
-        int longestPlayer = longestCon(board, playerIndex), longestEnemy = longestCon(board, enemyIndex);
-
-        if(board.getLastPlayerIndex() == playerIndex){
-            if(longestPlayer == 4 && longestEnemy < 3) value += 300;
-        }else{
-            if(longestEnemy == 4) value -= 1000;
-        }
+        if(countP == 4) value += 10000;
+        if(countE == 4) value -= 10000;
+        if(countP == 3 && countE == 0) value += 900;
+        if(countP == 2 && countE == 0) value += 100;
+        if(countE == 3 && countP == 0) value -= 1200;
+        if(countE == 2 && countP == 0) value -= 100;
 
 
         return value;
     }
 
+    private int utility(Board board){
+        int value = 0;
+        int[][] state = board.getState();
+
+        for(int col = 0; col < boardSize[1]; ++col){
+            for(int i = 0; i < boardSize[0] - 3; ++i) {
+                int pCount = getRockCount(new int[]{state[i][col], state[i+1][col], state[i+2][col],state[i+3][col]}, playerIndex);
+                int eCount = getRockCount(new int[]{state[i][col], state[i+1][col], state[i+2][col],state[i+3][col]}, enemyIndex);
+                value += score(pCount, eCount);
+            }
+        }
+
+        for(int row = 0; row < boardSize[0]; ++row){
+            for(int i = 0; i < boardSize[1] - 3; ++i){
+                int pCount = getRockCount(new int[]{state[row][i], state[row][i + 1], state[row][i + 2], state[row][i + 3]}, playerIndex);
+                int eCount = getRockCount(new int[]{state[row][i], state[row][i + 1], state[row][i + 2], state[row][i + 3]}, enemyIndex);
+                value += score(pCount, eCount);
+            }
+        }
+
+        for(int row = 0; row < boardSize[0] - 3; ++row){
+            for(int col = 0; col < boardSize[1] - 3; ++col){
+                int pCount = getRockCount(new int[]{state[row][col], state[row+1][col+1],state[row+2][col+2],state[row+3][col+3]}, playerIndex);
+                int eCount = getRockCount(new int[]{state[row][col], state[row+1][col+1],state[row+2][col+2],state[row+3][col+3]}, enemyIndex);
+                value += score(pCount, eCount);
+            }
+        }
+
+        for(int row = boardSize[0] - 1; row > 3; --row){
+            for(int col = 0; col < boardSize[1] - 3; ++col){
+                int pCount = getRockCount(new int[]{state[row][col], state[row - 1][col + 1], state[row - 2][col + 2], state[row - 3][col + 3]}, playerIndex);
+                int eCount = getRockCount(new int[]{state[row][col], state[row - 1][col + 1], state[row - 2][col + 2], state[row - 3][col + 3]}, enemyIndex);
+                value += score(pCount, eCount);
+            }
+        }
+        return value;
+    }
+
     private int[] minScore(Board board, int alfa, int beta, int depth){
-        int val = board.getLastPlayerColumn();
-        if(longestCon(board, playerIndex) == 4 || longestCon(board, enemyIndex) == 4 || depth == MAX_DEPTH) return new int[]{board.getLastPlayerColumn(), utility(board)};
+        if(depth == MAX_DEPTH || board.gameEnded()) return new int[]{board.getLastPlayerColumn(), utility(board)};
         int[] v = new int[]{1 ,Integer.MAX_VALUE};
         for(int col: board.getValidSteps()){
             Board testBoard = new Board(board);
@@ -64,7 +88,7 @@ public class StudentPlayer extends Player{
     }
 
     private int[] maxScore(Board board, int alfa, int beta, int depth){
-        if (longestCon(board, playerIndex) == 4 || longestCon(board, enemyIndex) == 4 || depth == MAX_DEPTH) return new int[]{board.getLastPlayerColumn(), utility(board)};
+        if(board.gameEnded() || depth == MAX_DEPTH) return new int[]{board.getLastPlayerColumn(), utility(board)};
         int[] v = new int[]{1, -Integer.MAX_VALUE};
         for(int col: board.getValidSteps()){
             Board testBoard = new Board(board);
@@ -78,99 +102,5 @@ public class StudentPlayer extends Player{
         }
 
         return v;
-    }
-
-    private boolean isNInARow(int row, int col, int playerIndex, int nToConnect, int[][] state) {
-        int nInARow = 0;
-
-        int startCol = Math.max(0, col - nToConnect + 1);
-        int endCol = Math.min(boardSize[1], col + nToConnect);
-        boolean badVal = false;
-    
-        if(startCol == 0 && endCol != boardSize[1]) badVal = state[row][endCol] != 0;
-        if(endCol == boardSize[1] && startCol != 0) badVal = state[row][startCol - 1] != 0;
-        if(startCol != 0 && endCol != boardSize[1]) badVal = state[row][startCol - 1] != 0 && state[row][endCol] != 0;
-
-        if(badVal) return false;
-        if(startCol != 0 && endCol != boardSize[1] && (state[row][startCol - 1] != 0 && state[row][endCol] != 0)) return false;
-        for (int c = startCol; c < endCol; c++) {
-            if (state[row][c] == playerIndex) {
-                nInARow++;
-                if (nInARow == nToConnect) {
-                    return true;
-                }
-            } else
-                nInARow = 0;
-        }
-        return false;
-    }
-
-    private boolean isNInACol(int row, int col, int playerIndex, int nToConnect, int[][] state) {
-        int nInACol = 0;
-
-        int startRow = Math.max(0, row - nToConnect + 1);
-        int endRow = Math.min(boardSize[0], row + nToConnect);
-        
-        boolean badVal = false;
-    
-        if(startRow == 0 && endRow != boardSize[0]) badVal = state[endRow][col] != 0;
-        if(endRow == boardSize[0] && startRow != 0) badVal = state[startRow - 1][col] != 0;
-        if(startRow != 0 && endRow != boardSize[0]) badVal = state[startRow - 1][col] != 0 && state[endRow][col] != 0;
-
-        if(badVal) return false;
-
-        for (int r = startRow; r < endRow; r++) {
-            if (state[r][col] == playerIndex) {
-                nInACol++;
-                if (nInACol == nToConnect) {
-                    return true;
-                }
-            } else
-                nInACol = 0;
-        }
-        return false;
-    }
-
-    private boolean isNDiagonally(int row, int col, int playerIndex, int nToConnect, int[][] state) {
-        int nInADiagonal = 0;
-
-        int stepLeftUp = Math.min(nToConnect - 1, Math.min(row, col));
-        int stepRightDown = Math.min(nToConnect, Math.min(boardSize[0] - row, boardSize[1] - col));
-
-        if ((stepLeftUp + stepRightDown) < nToConnect)
-            return false;
-
-        for (int diagonalStep = -stepLeftUp; diagonalStep < stepRightDown; diagonalStep++) {
-            if (state[row + diagonalStep][col + diagonalStep] == playerIndex) {
-                nInADiagonal++;
-                if (nInADiagonal == nToConnect) {
-                    return true;
-                }
-            } else {
-                nInADiagonal = 0;
-            }
-        }
-        return false;
-    }
-
-    private boolean isNSkewDiagonally(int row, int col, int playerIndex, int nToConnect, int[][] state) {
-        int nInASkewDiagonal = 0;
-
-        int stepLeftDown = Math.min(nToConnect - 1, Math.min(boardSize[0] - row - 1, col));
-        int stepRightUp = Math.min(nToConnect, Math.min(row + 1, boardSize[1] - col));
-
-        if ((stepRightUp + stepLeftDown) < nToConnect)
-            return false;
-
-        for (int skewDiagonalStep = -stepLeftDown; skewDiagonalStep < stepRightUp; skewDiagonalStep++) {
-            if (state[row - skewDiagonalStep][col + skewDiagonalStep] == playerIndex) {
-                nInASkewDiagonal++;
-                if (nInASkewDiagonal == nToConnect) {
-                    return true;
-                }
-            } else
-                nInASkewDiagonal = 0;
-        }
-        return false;
     }
 }
